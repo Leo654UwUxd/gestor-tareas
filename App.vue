c3c4e8fed2e62cb18c0fab67400f4d1da275ad34
Leo654UwUxd <leonardomunoz479@gmail.com>
2025-11-23 17:11:15 -0400

    Add App.vue with TaskList component

---
+++ App.vue
@@ -0,0 +1,19 @@
+<template>
+  <h1>Gestor de Tareas</h1>
+  <TaskList />
+</template>
+
+<script>
+import TaskList from "./components/TaskList.vue";
+
+export default {
+  components: { TaskList }
+};
+</script>
+
+<style>
+body {
+  font-family: Arial, sans-serif;
+  padding: 20px;
+}
+</style>
